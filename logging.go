9f78304de5c012b4676758d7cf132351324a85da
Ngoc Phu Truong <ngocphu811@live.com>
2021-01-03 15:49:24 +0700

    test

--- logging.go
+++ logging.go
@@ -1,7 +1,7 @@
 package mediadevices
 
 import (
-	"github.com/pion/mediadevices/internal/logging"
+	"github.com/kmtekvn/mediadevices/internal/logging"
 )
 
 var logger = logging.NewLogger("mediadevices")
